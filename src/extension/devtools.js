5ec4627decfad96ee85a60b7caa302f4a17eb491
Fernando Serrano <fernandojsg@gmail.com>
2019-10-28 17:06:27 +0100

    Fix icon on developer tools

--- src/extension/devtools.js
+++ src/extension/devtools.js
@@ -3,6 +3,6 @@ var globalBrowser =  chrome || browser;
 // @todo Import browser polyfill
 globalBrowser.devtools.panels.create(
   "ECSY",
-  "/assets/icon_128.png",
+  "/assets/icon_128_detected.png",
   "/src/app/index.html"
 );
